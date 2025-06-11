7b3117f1b6ce43fb6959b14902fe0f3bddb581aa
planmoni <planmoniapp@gmail.com>
2025-06-11 21:25:26 +0100

    Updated amount.tsx

--- app/create-payout/amount.tsx
+++ app/create-payout/amount.tsx
@@ -171,7 +171,7 @@ const createStyles = (colors: any) => StyleSheet.create({
     alignItems: 'center',
     backgroundColor: colors.backgroundTertiary,
     borderRadius: 12,
-    padding: 4,
+    paddingLeft: 4,
     marginBottom: 16,
     borderWidth: 1,
     borderColor: colors.border,
