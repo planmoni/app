5bc6b718b2493d16e394e2b639534bcc2196ec07
Planmoni App <planmoniapp@gmail.com>
2025-06-13 12:30:31 +0100

    Updated index.tsx

--- app/index.tsx
+++ app/index.tsx
@@ -219,7 +219,7 @@ const createStyles = (colors: any, isDark: boolean, responsive: any) => StyleShe
     alignItems: 'center',
   },
   slideImage: {
-    width: '60%',
+    width: '100%',
     height: responsive.imageHeight,
     marginBottom: responsive.verticalPadding / 2,
   },
