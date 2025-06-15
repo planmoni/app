3412841e0078b0b6681236f8fa0507dc4e60f548
Planmoni App <planmoniapp@gmail.com>
2025-06-16 00:21:19 +0100

    Updated index.tsx

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -243,7 +243,7 @@ export default function HomeScreen() {
       <SafeAreaView style={styles.container} edges={['top']}>
         <PlanmoniLoader 
           blurBackground={true} 
-          size="medium" 
+          size="large" 
         />
       </SafeAreaView>
     );
