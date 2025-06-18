3b239a9ca70e17474ec83c81c848fdddba2339ca
Planmoni App <planmoniapp@gmail.com>
2025-06-18 08:13:35 +0100

    Updated index.tsx

--- app/index.tsx
+++ app/index.tsx
@@ -25,7 +25,7 @@ const SLIDES = [
   },
   {
     id: '2',
-    title: 'Create a personalize salary structure',
+    title: 'Personalize salary structure',
     description: 'Split deposits into small, scheduled weekly, bi-weekly or monthly payouts - just like salary',
     image: require('@/assets/images/PayYourselfOnTime.png'),
     icon: Calendar,
@@ -34,7 +34,7 @@ const SLIDES = [
   },
   {
     id: '3',
-    title: 'Create a steady income flow',
+    title: 'Steady income flow',
     description: 'Secure your money, get paid on a schedule & say goodbye to irregular income. ',
     image: require('@/assets/images/SmartSavings.png'),
     icon: Wallet,
