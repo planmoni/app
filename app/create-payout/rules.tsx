1da262103cb52d7d7ba1c42b4fc306ebb60e6d24
Planmoni App <planmoniapp@gmail.com>
2025-06-23 12:57:49 +0100

    Updated rules.tsx

--- app/create-payout/rules.tsx
+++ app/create-payout/rules.tsx
@@ -227,7 +227,7 @@ const createStyles = (colors: any, isDark: boolean, isSmallScreen: boolean) => S
     paddingTop: 0,
   },
   title: {
-    fontSize: isSmallScreen ? 22 : 24,
+    fontSize: isSmallScreen ? 15 : 18,
     fontWeight: '600',
     color: colors.text,
     marginBottom: 8,
