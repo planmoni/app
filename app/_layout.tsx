adf86f22a4b6f8eac1ec930defab976522db90e1
Planmoni App <planmoniapp@gmail.com>
2025-06-26 08:32:43 +0100

    Updated _layout.tsx

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -139,7 +139,7 @@ const styles = StyleSheet.create({
   errorMessage: {
     fontSize: 16,
     color: '#666',
-    marginBottom: 16,
+    marginBottom: 16,i
     textAlign: 'center',
     lineHeight: 24,
   },
