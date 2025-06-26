9c888087065a7ca86f2b91811458a7877d3f8c48
Planmoni App <planmoniapp@gmail.com>
2025-06-26 08:14:17 +0100

    Updated app.config.js

--- app.config.js
+++ app.config.js
@@ -14,6 +14,6 @@ export default {
       PAYSTACK_SECRET_KEY: process.env.PAYSTACK_SECRET_KEY || '',
       DOJAH_APP_ID: process.env.DOJAH_APP_ID || '',
       DOJAH_PRIVATE_KEY: process.env.DOJAH_PRIVATE_KEY || '',
-    },t
+    },
   },
 };
