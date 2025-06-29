ea55922aa6161fa0e714bd5c5690eb9670fcf3a9
Planmoni App <planmoniapp@gmail.com>
2025-06-29 13:05:27 +0100

    Updated BannerCarousel.tsx

--- components/BannerCarousel.tsx
+++ components/BannerCarousel.tsx
@@ -202,7 +202,7 @@ const styles = StyleSheet.create({
   image: {
     width: '100%',
     height: '100%',
-    borderRadius: 16,
+    borderRadius: 8,
   },
   overlay: {
     ...StyleSheet.absoluteFillObject,
