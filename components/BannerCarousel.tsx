bf9edcd56b9143645f6327a61095b6d9cdd79596
Planmoni App <planmoniapp@gmail.com>
2025-06-29 13:10:45 +0100

    Updated BannerCarousel.tsx

--- components/BannerCarousel.tsx
+++ components/BannerCarousel.tsx
@@ -38,7 +38,7 @@ export default function BannerCarousel({
   autoPlayInterval = 1000,
   showPagination = true,
   showControls = false,
-  height = 200,
+  height = 900,
 }: BannerCarouselProps) {
   const { colors, isDark } = useTheme();
   const [banners, setBanners] = useState<Banner[]>([]);
