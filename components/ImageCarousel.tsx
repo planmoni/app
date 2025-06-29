6aab6c798c8e02785a62a8b88d2731cdfd2ab7b2
Planmoni App <planmoniapp@gmail.com>
2025-06-29 15:43:44 +0100

    Updated ImageCarousel.tsx

--- components/ImageCarousel.tsx
+++ components/ImageCarousel.tsx
@@ -183,7 +183,7 @@ export default function ImageCarousel({
             <Image
               source={{ uri: image.image_url }}
               style={[styles.image]}
-              resizeMode="fit"
+              resizeMode="cover"
               width={310}
               height={150}
               onError={() =>
