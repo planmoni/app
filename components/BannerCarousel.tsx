4ca61f28b1eefe8002eb089b2984b45e2ddebcf8
Planmoni App <planmoniapp@gmail.com>
2025-06-29 13:08:16 +0100

    Updated BannerCarousel.tsx

--- components/BannerCarousel.tsx
+++ components/BannerCarousel.tsx
@@ -156,7 +156,7 @@ export default function BannerCarousel({
         {banners.map((banner) => (
           <Pressable
             key={banner.id}
-            style={[styles.slide, { width: screenWidth - 50 }]}
+            style={[styles.slide, { width: screenWidth - 30 }]}
             onPress={() => handleBannerPress(banner)}
           >
             <Image
