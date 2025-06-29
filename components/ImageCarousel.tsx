094cd86ce5325107b6c5f3d4843a8c5df4a6df86
Planmoni App <planmoniapp@gmail.com>
2025-06-29 15:39:47 +0100

    Updated ImageCarousel.tsx

--- components/ImageCarousel.tsx
+++ components/ImageCarousel.tsx
@@ -40,7 +40,7 @@ interface ImageCarouselProps {
 }
 
 const { width: screenWidth } = Dimensions.get('window');
-const SLIDE_MARGIN = 10;
+const SLIDE_MARGIN = 5;
 const SLIDE_WIDTH = screenWidth - SLIDE_MARGIN * 2;
 const SNAP_INTERVAL = SLIDE_WIDTH + SLIDE_MARGIN;
 
