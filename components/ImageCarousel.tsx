f2db52799d74621b25c264c654a93045c194c2ed
Planmoni App <planmoniapp@gmail.com>
2025-06-29 16:07:45 +0100

    Updated ImageCarousel.tsx

--- components/ImageCarousel.tsx
+++ components/ImageCarousel.tsx
@@ -255,7 +255,7 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     marginTop: 5,
     marginBottom: 5,
-    gap: 8,
+    gap: 5,
   },
   captionContainer: {
     position: 'absolute',
