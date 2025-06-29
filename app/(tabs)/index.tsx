44d9704a5ca203fa04a9fd224bbf04a9c1e81f6e
Planmoni App <planmoniapp@gmail.com>
2025-06-29 12:03:31 +0100

    Updated package.json

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -5,7 +5,6 @@ import InitialsAvatar from '@/components/InitialsAvatar';
 import PlanmoniLoader from '@/components/PlanmoniLoader';
 import CountdownTimer from '@/components/CountdownTimer';
 import PendingActionsCard from '@/components/PendingActionsCard';
-import BannerCarousel from '@/components/BannerCarousel';
 import { useRoute } from '@react-navigation/native';
 import { router, useLocalSearchParams } from 'expo-router';
 import { ArrowDown, ArrowDownRight, ArrowRight, ArrowUpRight, Calendar, ChevronDown, ChevronRight, ChevronUp, Eye, EyeOff, Lock, Pause, Play, Plus, Send, Wallet } from 'lucide-react-native';
@@ -341,13 +340,7 @@ export default function HomeScreen() {
           </View>
         </Card>
 
-        {/* Banner Carousel */}
-        <BannerCarousel 
-          autoPlay={true}
-          autoPlayInterval={5000}
-          showPagination={true}
-          height={180}
-        />
+        
 
         <PendingActionsCard />
 
