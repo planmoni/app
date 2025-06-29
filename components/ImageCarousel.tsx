b2b3aef66d60413a0ce2c60e8437c7111cb38173
Planmoni App <planmoniapp@gmail.com>
2025-06-29 15:22:29 +0100

    Updated ImageCarousel.tsx

--- components/ImageCarousel.tsx
+++ components/ImageCarousel.tsx
@@ -48,7 +48,7 @@ export default function ImageCarousel({
   autoPlay = true,
   autoPlayInterval = 5000,
   showPagination = true,
-  height = 200,
+  height = 116,
   images: propImages,
 }: ImageCarouselProps) {
   const { colors, isDark } = useTheme();
